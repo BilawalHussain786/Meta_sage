bb7c8f4a03d85a43d2853207ad33fc4b0e1808a5
BilawalHussain786 <bilawalgujjar481854@gmail.com>
2024-03-21 14:45:36 +0500

    optimize productivity

--- lib/ui/widgets/optimize_wiget.dart
+++ lib/ui/widgets/optimize_wiget.dart
@@ -15,22 +15,26 @@ class _OptimizeProductsState extends State<OptimizeProducts> {
     dynamic size = MediaQuery.of(context).size;
     return Column(
       children: [
-        Text("Optimized for productivity"),
+        Text(
+          "Optimized for productivity",
+          style: TextStyle(fontSize: 80, fontWeight: FontWeight.bold),
+        ),
         SizedBox(height: 200),
         Container(
-          width: 800,
           //width: size.width * 0.7,
-          height: 600,
+
           // height: size.height * 0.9,
           color: ConstColor.white,
           child: Stack(
             clipBehavior: Clip.none,
             children: [
               Padding(
-                padding: const EdgeInsets.only(top: 15, bottom: 15),
+                padding: const EdgeInsets.all(15),
                 child: Image.asset(
                   ConstImages.speed1,
                   fit: BoxFit.cover,
+                  height: 600,
+                  width: 800,
                   //width: size.width * 1,
                 ),
               ),
@@ -38,39 +42,50 @@ class _OptimizeProductsState extends State<OptimizeProducts> {
                 top: -100,
                 left: -180,
                 child: Container(
-                  color: ConstColor.white,
-                  child: Image.asset(
-                    ConstImages.speed3,
-                    height: 300,
-                    width: 250,
+                  decoration: BoxDecoration(
+                      color: ConstColor.white,
+                      borderRadius: BorderRadius.circular(8)),
+                  child: Padding(
+                    padding: const EdgeInsets.all(15.0),
+                    child: Image.asset(
+                      ConstImages.speed3,
+                      height: 300,
+                      width: 250,
+                    ),
                   ),
                 ),
               ),
               Positioned(
                 bottom: -50,
-                left: -150,
+                left: -180,
                 child: Container(
-                  width: 400,
-                  height: 250,
-                  color: ConstColor.white,
-                  child: Image.asset(
-                    ConstImages.speed4,
-                    height: 200,
-                    width: 200,
+                  decoration: BoxDecoration(
+                      color: ConstColor.white,
+                      borderRadius: BorderRadius.circular(8)),
+                  child: Padding(
+                    padding: const EdgeInsets.all(15.0),
+                    child: Image.asset(
+                      ConstImages.speed4,
+                      width: 500,
+                      height: 250,
+                    ),
                   ),
                 ),
               ),
               Positioned(
-                bottom: -40,
+                bottom: -20,
                 right: -150,
                 child: Container(
-                  width: 250,
-                  height: 300,
-                  color: ConstColor.white,
-                  child: Image.asset(
-                    ConstImages.speed2,
-                    height: 200,
-                    width: 200,
+                  decoration: BoxDecoration(
+                      color: ConstColor.white,
+                      borderRadius: BorderRadius.circular(8)),
+                  child: Padding(
+                    padding: const EdgeInsets.all(15.0),
+                    child: Image.asset(
+                      ConstImages.speed2,
+                      width: 250,
+                      height: 300,
+                    ),
                   ),
                 ),
               ),
